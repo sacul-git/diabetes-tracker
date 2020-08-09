84ba65ea2f607120f984840a936a6f7af014139f
Lucas <lu.goldstone@gmail.com>
2020-08-09 21:05:15 +0100

    Add icon and other style changes

--- layouts/app_layout.py
+++ layouts/app_layout.py
@@ -1,6 +1,8 @@
 import dash_core_components as dcc
 import dash_html_components as html
 
+from app import app
+
 def serve_layout():
     return html.Div(
             children = [
@@ -8,7 +10,11 @@ def serve_layout():
                 html.Div(
                     className="header",
                     children=[
-                        html.H1("Header Title"),
+                        html.Img(
+                            className = "logo",
+                            src=app.get_asset_url("diabetes.svg")
+                        ),
+                        html.Span(className="app-title", children="Header Title"),
                         html.Div(
                             id = "header-logout",
                             className = "header-right",
@@ -23,6 +29,18 @@ def serve_layout():
                             )
                         ]
                     ),
-                html.Div(id="page-content", className = "below-header")
-                ]
+                html.Div(
+                    className="spacer",
+                    children = "good job you found me I'm hiding behind the header"
+                ),
+                html.Div(id="page-content", className = "page-content"),
+                html.Div(
+                    className="attribution",
+                    children=[
+                    "Icons made by ",
+                    html.A(href="http://www.freepik.com/", children="Freepick"),
+                    " from ",
+                    html.A(href="https://www.flaticon.com/", children="flaticon.com")
+                    ])
+                ],
             )
